19caaaad4ac07a9eec512bc2a3dd72616f5a78d8
Christos Deretzis <deretzischristos@gmail.com>
2022-12-11 22:24:51 +0200

    Remove unused imports

--- src/test/groovy/com/deretzis/hotel_booking_backend/controller/UserControllerImplSpec.groovy
+++ src/test/groovy/com/deretzis/hotel_booking_backend/controller/UserControllerImplSpec.groovy
@@ -2,22 +2,16 @@ package com.deretzis.hotel_booking_backend.controller
 
 import com.deretzis.hotel_booking_backend.configuration.SecurityConfiguration
 import com.deretzis.hotel_booking_backend.dto.CreateUserDto
-import com.deretzis.hotel_booking_backend.service.UserService
 import com.deretzis.hotel_booking_backend.service.UserServiceImpl
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.spockframework.spring.SpringBean
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
-import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders
-import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.ResultMatcher
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
-import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import spock.lang.Specification
 
 import java.time.LocalDate
