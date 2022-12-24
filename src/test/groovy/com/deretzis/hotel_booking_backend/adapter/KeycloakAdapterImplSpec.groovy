da322fdf558bc2c160bd0995afd464bbf15b98ec
Christos Deretzis <deretzischristos@gmail.com>
2022-12-24 11:16:25 +0200

    Remove unused imports

---
+++ src/test/groovy/com/deretzis/hotel_booking_backend/adapter/KeycloakAdapterImplSpec.groovy
@@ -0,0 +1,8 @@
+package com.deretzis.hotel_booking_backend.adapter
+
+import org.springframework.test.context.ContextConfiguration
+import spock.lang.Specification
+
+@ContextConfiguration(classes = [KeycloakAdapterImpl])
+class KeycloakAdapterImplSpec extends Specification {
+}
